ae2b477e5c4246f4ce6ee40d86a940a5e0b4a3c5
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2018-03-09 21:17:04 +0100

    FeatureCounts unit test (#22)

---
+++ src/test/scala/pl/edu/pw/ii/biodatageeks/tests/FeatureCountsTestSuite.scala
@@ -0,0 +1,63 @@
+package pl.edu.pw.ii.biodatageeks.tests
+
+import com.holdenkarau.spark.testing.{DataFrameSuiteBase, SharedSparkContext}
+import htsjdk.samtools.ValidationStringency
+import org.apache.hadoop.io.LongWritable
+import org.biodatageeks.rangejoins.IntervalTree.IntervalTreeJoinStrategyOptim
+import org.scalatest.{BeforeAndAfter, FunSuite}
+import org.seqdoop.hadoop_bam.{BAMInputFormat, SAMRecordWritable}
+import org.seqdoop.hadoop_bam.util.SAMHeaderReader
+
+case class Region(contigName:String,start:Int,end:Int)
+
+class FeatureCountsTestSuite extends FunSuite with DataFrameSuiteBase with BeforeAndAfter with SharedSparkContext{
+
+
+  before{
+    System.setSecurityManager(null)
+    spark.experimental.extraStrategies = new IntervalTreeJoinStrategyOptim(spark) :: Nil
+  }
+
+  test ("Feature counts for chr1:20138-20294") {
+    val query ="""SELECT count(*),targets.contigName,targets.start,targets.end
+              FROM reads JOIN targets
+        |ON (
+        |  targets.contigName=reads.contigName
+        |  AND
+        |  reads.end >= targets.start
+        |  AND
+        |  reads.start <= targets.end
+        |)
+        |GROUP BY targets.contigName,targets.start,targets.end
+        |having contigName='chr1' AND    start=20138 AND  end=20294""".stripMargin
+
+
+    spark
+      .sparkContext
+      .hadoopConfiguration.set(SAMHeaderReader.VALIDATION_STRINGENCY_PROPERTY, ValidationStringency.SILENT.toString)
+
+    val alignments = spark
+      .sparkContext
+      .newAPIHadoopFile[LongWritable, SAMRecordWritable, BAMInputFormat](getClass.getResource("/NA12878.slice.bam").getPath)
+      .map(_._2.get)
+      .map(r => Region(r.getContig, r.getStart, r.getEnd))
+
+    val reads = spark
+      .sqlContext
+      .createDataFrame(alignments)
+    reads
+      .createOrReplaceTempView("reads")
+
+    val targets = spark
+      .sqlContext
+      .createDataFrame(Array(Region("chr1",20138,20294)))
+    targets
+      .createOrReplaceTempView("targets")
+
+    assert(spark.sql(query).first().getLong(0) === 1484L)
+  }
+
+
+
+}
+
