eb90a20f17a0fa19ec2a2a94ce8150e81ccf9ad6
mwiewior <marek.wiewiorka@gmail.com>
2020-08-12 08:56:49 +0200

    Extended debug

--- src/main/scala/org/biodatageeks/sequila/pileup/model/ReadQualSummary.scala
+++ src/main/scala/org/biodatageeks/sequila/pileup/model/ReadQualSummary.scala
@@ -13,10 +13,15 @@ case class ReadQualSummary (start: Int, end: Int,
   def getBaseQualityForPosition(position: Int): Byte = {
     try {
       qualsArray(relativePosition(position))
+
     }
     catch {
       case e: Exception => {
-        log.error(s"ReadName: ${debugReadName}, position: ${position}")
+        log.error(s"ReadName: ${debugReadName} position: ${position}, start: ${start}, end: ${`end`} " +
+          s"insertPos: ${cigarDerivedConf.indelPositions.insertPositions.mkString(",")}" +
+          s"delPos: ${cigarDerivedConf.indelPositions.delPositions.mkString(",")}," +
+          s"hasClip: ${cigarDerivedConf.hasClip}, hasIndel: ${cigarDerivedConf.hasIndel}" +
+          s"hasDel: ${cigarDerivedConf.hasDel}, leftClipLength:${cigarDerivedConf.leftClipLength}")
         'E'.toByte
       }
     }
