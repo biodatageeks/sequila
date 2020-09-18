cbbb1db884377a33e25b10562b5dd275f02de396
mwiewior <marek.wiewiorka@gmail.com>
2020-09-18 21:07:50 +0200

    Fixing build doc

--- build_docs.sh
+++ build_docs.sh
@@ -3,7 +3,7 @@
 
 cd docs
 echo $VERSION
-if [[ echo $VERSION | grep -Eq "SNAPSHOT$" ]]; then
+if  echo $VERSION | grep -Eq "SNAPSHOT$"; then
     docker build --no-cache -t zsi-bio/bdg-sequila-snap-doc .
     if [ $(docker ps | grep bdg-sequila-snap-doc | wc -l) -gt 0 ]; then docker stop bdg-sequila-snap-doc && docker rm bdg-sequila-snap-doc; fi
     docker run -p 81:80 -d --name bdg-sequila-snap-doc zsi-bio/bdg-sequila-snap-doc
