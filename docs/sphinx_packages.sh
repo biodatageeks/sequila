f98e98617bf6a310c8eb22ba4518de49ca161e38
mwiewior <marek.wiewiorka@gmail.com>
2021-04-06 15:06:49 +0200

    Adding documentation for Custom interval structure

--- docs/sphinx_packages.sh
+++ docs/sphinx_packages.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
+mkdir -p venv/sphinx
+virtualenv venv/sphinx
+source venv/sphinx/bin/activate
 #core
 #pip install -U sphinx
 pip install -U sphinx==1.7.9
@@ -8,5 +12,5 @@ pip install -U sphinx_rtd_theme
 pip install -U rst2pdf
 #https://github.com/rst2pdf/rst2pdf/pull/770/files
 pip install -U sphinxcontrib-github_ribbon
-pip install sphinxcontrib-bibtex
+pip install -U "sphinxcontrib-bibtex<2.0.0"
 
