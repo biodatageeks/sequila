ebbafecc99f8891139af44325eb5e538871fd45a
agaszmurlo <amszmurlo@gmail.com>
2021-10-11 15:05:20 +0200

    wip: assuring correct pileup calculations

--- src/main/scala/org/biodatageeks/sequila/pileup/model/ContigAggregateRDD.scala
+++ src/main/scala/org/biodatageeks/sequila/pileup/model/ContigAggregateRDD.scala
@@ -11,6 +11,7 @@ import org.biodatageeks.sequila.pileup.model.Alts._
 import org.biodatageeks.sequila.pileup.model.Quals._
 import org.biodatageeks.sequila.pileup.partitioning.PartitionBounds
 import org.slf4j.{Logger, LoggerFactory}
+import scala.util.control.Breaks._
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -23,10 +24,10 @@ object AggregateRDDOperations {
 case class AggregateRDD(rdd: RDD[ContigAggregate]) {
   val logger: Logger = LoggerFactory.getLogger(this.getClass.getCanonicalName)
 
-  def isInPartitionRange(i: Int, startPos: Int, contig: String, bound: PartitionBounds): Boolean = {
-//    if (contig == bound.contigStart && contig == bound.contigEnd)
-//      i + startPos >= bound.postStart && i + startPos <= bound.posEnd
-//    else
+  def isInPartitionRange(currPos: Int, contig: String, bound: PartitionBounds): Boolean = {
+    if (contig == bound.contigStart && contig == bound.contigEnd)
+      currPos >= bound.postStart - 1 && currPos <= bound.posEnd
+    else
       true
   }
 
@@ -38,7 +39,7 @@ case class AggregateRDD(rdd: RDD[ContigAggregate]) {
       PileupProjection.setContigMap(contigMap)
 
       part.map { agg => {
-        var cov, ind, i = 0
+        var cov, ind, i, currPos = 0
         val allPos = false
         val maxLen = agg.calculateMaxLength(allPos)
         val result = new Array[InternalRow](maxLen)
@@ -48,42 +49,48 @@ case class AggregateRDD(rdd: RDD[ContigAggregate]) {
         val contig = agg.contig
         val bases = reference.getBasesFromReference(contigMap(agg.contig), agg.startPosition, agg.startPosition + agg.events.length - 1)
 
-        while ( i < agg.events.length) { // repartition change -> no shrinking, we have to go through whole array
-          cov += agg.events(i)
-          if (isInPartitionRange(i, startPosition, contig, partitionBounds)) {
-
-                  if (prev.hasAlt) {
-                    addBaseRecord(result, ind, agg, bases, i, prev, conf)
-                    ind += 1;
-                    prev.reset(i)
-                    if (agg.hasAltOnPosition(i+startPosition))
-                      prev.alt=agg.alts(i+startPosition)
-                  }
-                  else if (agg.hasAltOnPosition(i+startPosition)) { // there is ALT in this posiion
-                    if (prev.isNonZeroCoverage) { // there is previous group non-zero group -> convert it
-                      addBlockRecord(result, ind, agg, bases, i, prev)
-                      ind += 1;
-                      prev.reset(i)
-                    } else if (prev.isZeroCoverage) // previous ZERO group, clear block
-                    prev.reset(i)
-                    prev.alt = agg.alts(i+startPosition)
-                  } else if (isEndOfZeroCoverageRegion(cov, prev.cov, i)) { // coming back from zero coverage. clear block
-                    prev.reset(i)
-                  } else if (isChangeOfCoverage(cov, prev.cov, i) || isStartOfZeroCoverageRegion(cov, prev.cov)) { // different cov, add to output previous group
-                    addBlockRecord(result, ind, agg, bases, i, prev)
-                    ind += 1;
-                    prev.reset(i)
-                  }  else if (i==agg.shrinkedEventsArraySize-1) { // last item -> convert it
-                    addBlockRecord(result, ind, agg, bases, i, prev)
-                    ind += 1;
-                    prev.reset(i)
-                  }
-          }
-
-          prev.cov = cov;
-          prev.len = prev.len + 1;
-          i += 1
-        }
+breakable {
+  while (i < agg.events.length) { // repartition change -> no shrinking, we have to go through whole array
+    currPos = i + startPosition
+    cov += agg.events(i)
+    if (contig == "MT" && currPos == 7931 && startPosition == 7831)
+      println
+    if (isInPartitionRange(currPos, contig, partitionBounds)) {
+      if (currPos == partitionBounds.postStart - 1)
+        prev.reset(i)
+      if (prev.hasAlt) {
+        addBaseRecord(result, ind, agg, bases, i, prev, conf)
+        ind += 1;
+        prev.reset(i)
+        if (agg.hasAltOnPosition(currPos))
+          prev.alt = agg.alts(currPos)
+      }
+      else if (agg.hasAltOnPosition(currPos)) { // there is ALT in this posiion
+        if (prev.isNonZeroCoverage) { // there is previous group non-zero group -> convert it
+          addBlockRecord(result, ind, agg, bases, i, prev)
+          ind += 1;
+          prev.reset(i)
+        } else if (prev.isZeroCoverage) // previous ZERO group, clear block
+          prev.reset(i)
+        prev.alt = agg.alts(currPos)
+      } else if (isEndOfZeroCoverageRegion(cov, prev.cov, i)) { // coming back from zero coverage. clear block
+        prev.reset(i)
+      } else if (isChangeOfCoverage(cov, prev.cov, i) || isStartOfZeroCoverageRegion(cov, prev.cov)) { // different cov, add to output previous group
+        addBlockRecord(result, ind, agg, bases, i, prev)
+        ind += 1;
+        prev.reset(i)
+      } else if (currPos == partitionBounds.posEnd) { // last item -> convert it
+        addBlockRecord(result, ind, agg, bases, i, prev)
+        ind += 1;
+        prev.reset(i)
+        break
+      }
+    }
+    prev.cov = cov;
+    prev.len = prev.len + 1;
+    i += 1
+  } // while
+}
 
         if (ind < maxLen) result.take(ind) else result
       }
