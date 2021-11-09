6bbbab7e3f640f52e8c41d4fc5d798e11e9309e7
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2021-11-09 18:00:56 +0100

    Fix/clean contig (#98)
    * dataquality func changed
    
    * minor fix
    
    Co-authored-by: agaszmurlo <amszmurlo@gmail.com>

--- src/main/scala/org/biodatageeks/sequila/utils/DataQualityFuncs.scala
+++ src/main/scala/org/biodatageeks/sequila/utils/DataQualityFuncs.scala
@@ -6,13 +6,38 @@ object DataQualityFuncs {
     * @param contig
     * @return
     */
-  def cleanContig (contig : String) = {
-    if(contig != null)
-    contig
-      .replaceFirst("^(?i)chrM$", "chrMT")
-      .replaceFirst("^(?i)chr","")
-      .replaceFirst("^M$", "MT")
-    else
-      contig
+  def cleanContig (contig : String): String = {
+    contig match {
+      case "chr1" => "1"
+      case "chr2" => "2"
+      case "chr3" => "3"
+      case "chr4" => "4"
+      case "chr5" => "5"
+      case "chr6" => "6"
+      case "chr7" => "7"
+      case "chr8" => "8"
+      case "chr9" => "9"
+      case "chr10" => "10"
+      case "chr11" => "11"
+      case "chr12" => "12"
+      case "chr13" => "13"
+      case "chr14" => "14"
+      case "chr15" => "15"
+      case "chr16" => "16"
+      case "chr17" => "17"
+      case "chr18" => "18"
+      case "chr19" => "19"
+      case "chr20" => "20"
+      case "chr21" => "21"
+      case "chr22" => "22"
+      case "chrX" => "X"
+      case "chrY" => "Y"
+      case "chrM" => "MT"
+      case "chrMT" => "MT"
+      case "M" => "MT"
+      case c if c !=null && c.startsWith("chr") => contig.replace("chr","")
+      case _ => contig
+    }
   }
+
 }
