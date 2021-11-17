227a06985c16d4ef4396b1a4362b061a0795fc00
Marek Wiewiórka <marek.wiewiorka@gmail.com>
2021-11-17 18:44:51 +0100

    Adding UT for Pqrquet/ORC pileup/cov IO

---
+++ src/test/scala/org/biodatageeks/sequila/tests/pileup/io/PileupSaveOutputTestSuite.scala
@@ -0,0 +1,79 @@
+package org.biodatageeks.sequila.tests.pileup.io
+
+import com.holdenkarau.spark.testing.RDDComparisons
+import org.apache.spark.sql.SequilaSession
+import org.biodatageeks.sequila.tests.pileup.PileupTestBase
+import org.biodatageeks.sequila.utils.SequilaRegister
+import org.scalatest.BeforeAndAfterAll
+
+import java.io.File
+import scala.reflect.io.Directory
+
+class PileupSaveOutputTestSuite
+  extends PileupTestBase
+    with BeforeAndAfterAll
+    with RDDComparisons{
+
+  val seed = randomString(10)
+  val baseOutputPath = s"/tmp/sequila_${seed}/"
+  val coveragePath = s"$baseOutputPath/coverage/"
+  val pileupPath = s"$baseOutputPath/pileup/"
+
+
+  test("Parquet save"){
+    val ss = SequilaSession(spark)
+    SequilaRegister.register(ss)
+    val parquetCoveragePath = s"$coveragePath/parquet/"
+    val parquetPileupPath = s"$pileupPath/parquet/"
+    val covRefDF = ss.sql(queryCoverage) //using rdd to not fight with nullability/schema just byte equality
+    covRefDF
+      .write
+      .parquet(parquetCoveragePath)
+    val covTestDF = ss
+      .read
+      .parquet(parquetCoveragePath)
+    assertRDDEquals(covRefDF.rdd, covTestDF.rdd)
+
+
+    val pileupRefDF = ss.sql(queryPileupWithQual) //using rdd to not fight with nullability/schema just byte equality
+    pileupRefDF
+      .write
+      .parquet(parquetPileupPath)
+    val pileupTestDF = ss
+      .read
+      .parquet(parquetPileupPath)
+    assertRDDEquals(pileupRefDF.rdd, pileupTestDF.rdd)
+  }
+
+  test("ORC save") {
+
+    val ss = SequilaSession(spark)
+    SequilaRegister.register(ss)
+    val orcCoveragePath = s"$coveragePath/orc/"
+    val orcPileupPath = s"$pileupPath/orc/"
+    val covRefDF = ss.sql(queryCoverage) //using rdd to not fight with nullability/schema just byte equality
+    covRefDF
+      .write
+      .orc(orcCoveragePath)
+    val covTestDF = ss
+      .read
+      .orc(orcCoveragePath)
+    assertRDDEquals(covRefDF.rdd, covTestDF.rdd)
+
+
+    val pileupRefDF = ss.sql(queryPileupWithQual) //using rdd to not fight with nullability/schema just byte equality
+    pileupRefDF
+      .write
+      .orc(orcPileupPath)
+    val pileupTestDF = ss
+      .read
+      .orc(orcPileupPath)
+    assertRDDEquals(pileupRefDF.rdd, pileupTestDF.rdd)
+
+  }
+
+  override def afterAll {
+    cleanup(baseOutputPath)
+  }
+
+}
